a05346136f0fe8492becf7c8cb2cd0f51eaa0315
BartKK <Bartkukulka@gmail.com>
2017-03-25 11:41:49 +0100

    seal object

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ Object.defineProperty(person, 'name', {
   configurable: true,
   writable: true
 });
+
+// Object.seal()
+// extensible i configurable ustawia na false
+
+(function(){
+  "use strict";
+var person = {
+  name: 'person'
+};
+console.log(Object.isExtensible(person)); // true
+console.log(Object.isSealed(person)); // false
+
+Object.seal(person);
+console.log(Object.isExtensible(person)); // false
+console.log(Object.isSealed(person)); // true
+
+person.sayHello = function () {
+  console.log(this.name);
+};
+
+// sprawdzimy istnienie metody
+console.log('sayHello' in person); //   false
+
+person.name = 'abcd';
+console.log(person.name); // ''
+})();
