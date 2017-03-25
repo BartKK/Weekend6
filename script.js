7cabb869acdc8e1c4abf6a9309263d29540e353c
BartKK <Bartkukulka@gmail.com>
2017-03-25 15:14:57 +0100

    Remove array duplicates and sumUp

--- script.js
+++ script.js
@@ -1,67 +1,16 @@
-//
-
-function Person(name) {
-  this.name = name;
-
-
-}
-
-Person.prototype.sayHello = function (name) {
-  console.log('Hello %s!', this.name)
-};
-
-var person1 = new Person('Ula');
-var person2 = new Person('Maria');
-
-console.log(person1.name, person2.name);
-
-person1.sayHello();
-person2.sayHello();
-
-
-(function(){
-  function Person(name) {
-    this.name = name;
-  }
-  Person.prototype.arrPerson = [];
-
-  var person1 = new Person('Ula');
-  var person2 = new Person('Maria');
-
-person1.arrPerson.push('czerwony');
-person2.arrPerson.push('niebieski');
-
-console.log(person1.arrPerson); // ['czerwony', 'niebieski']
-console.log(person2.arrPerson); // ['czerwony', 'niebieski']
-})();
 
 
 
-(function() {
-  function Person(name) {
-    this.name = name;
-  }
-var person1 = new Person('Zosia');
-var person2 = new Person('Tosia');
+var numbers = [5, 1, 2, 3, 0, 1, 5, 0, 2];
 
-Object.freeze(person1);
+var uniqueNumbers = [];
 
-Person.prototype.sayHi = function() {
-  console.log('Hi');
-};
-
-person1.sayHi(); // Hi
-person2.sayHi(); // Hi
-
-person1.name = 'asdasdasd'; // nie zadziała
-person2.name = 'asdaaszxczxcxzcsd'; // zadziała
-})();
-
-
-
-
-var numbers = [ 1, 2, 3, 4, 5, 6, 7];
-var result = numbers.reduce(function(a, b){
+    for(var i in numbers){
+        if(uniqueNumbers.indexOf(numbers[i]) === -1){
+            uniqueNumbers.push(numbers[i]);
+        }
+    }
+var result = uniqueNumbers.reduce(function(a, b){
   return a + b;
 });
 
