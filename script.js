a06024c0aa9417369cbe41e5ea81b49395f63e58
BartKK <Bartkukulka@gmail.com>
2017-03-25 11:20:59 +0100

    Value,Writable

--- script.js
+++ script.js
@@ -1,42 +1,17 @@
 // Atrybuty wspólne
 
-// [[Enumerable]], [[Configurable]]
-
-// Do zmiany tych właściwości używamy metody Object.defineProperty()
-
-var person = {
-  name: 'Ula'
-};
-
-// console.log(person.propertyIsEnumerable('name')); // true
-//
-// Object.defineProperty(person, 'name', {
-//   enumerable: false
-// });
-//
-//
-// console.log(person.propertyIsEnumerable('name')); // false
-
-
+// [[Value]], [[Writable]]
 (function(){
   "use strict";
-
-  var person = {
-    name: 'Ula'
-  };
-  console.log(person.propertyIsEnumerable('name')); //true
-
-  Object.defineProperty(person, 'name', {
-    configurable: false
-  });
-
-  Object.defineProperty(person, 'name', {
-    enumerable: false
-  });
-
-  console.log(perosn.propertyisEnumerable('name')); //false
-
-  Object.defineProperty(person, 'name', {
-    configurable: true
-  });
+var person = {
+  name: 'Konrad'
+};
 })();
+
+var person = {};
+Object.defineProperty(person, 'name', {
+  value: 'Paweł',
+  enumerable: true,
+  configurable: true,
+  writable: true
+});
