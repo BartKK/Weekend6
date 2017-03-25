610e3cdc0c8bfdef682fccbec0c47896a468a85b
BartKK <Bartkukulka@gmail.com>
2017-03-25 13:52:57 +0100

    prototype methods

--- script.js
+++ script.js
@@ -1,51 +1,53 @@
-// Konstruktory i prototypy
-
-function Computer() {
-  //
-}
-
-// new zwraca wartość
-var computer1 = new Computer;
-var computer2 = new Computer;
-
-
-console.log(computer1.constructor == Computer); // true
-
-
-////
+//
 
 function Person(name) {
   this.name = name;
-  this.sayHello = function () {
-    console.log('dasd');
-  };
+
+
 }
 
+Person.prototype.sayHello = function (name) {
+  console.log('Hello %s!', this.name)
+};
 
-var person1 = new Person('Piotr');
-var person2 = new Person('Konrad');
+var person1 = new Person('Ula');
+var person2 = new Person('Maria');
 
-console.log(person1.name); // 'Piotr'
+console.log(person1.name, person2.name);
+
+person1.sayHello();
+person2.sayHello();
+
+
+(function(){
+  function Person(name) {
+    this.name = name;
+  }
+  Person.prototype.arrPerson = [];
+
+  var person1 = new Person('Ula');
+  var person2 = new Person('Maria');
+
+person1.arrPerson.push('czerwony');
+person2.arrPerson.push('niebieski');
+
+console.log(person1.arrPerson); // ['czerwony', 'niebieski']
+console.log(person2.arrPerson); // ['czerwony', 'niebieski']
+})();
+
+
 
 (function() {
-  "use strict";
-
-  function Car(name) {
-    Object.defineProperty(this, 'name', {
-      get: function () {
-        return name
-      },
-      set: function (value) {
-        name = value
-      }
-    });
-
-       this.brrum = function () {
-         console.log("Bruuuum");
-       }
-
+  function Person(name) {
+    this.name = name;
   }
-
-  var car1 = new Car('Szybki');
-  car1.brrum(); // "Bruuuum"
+  //Definiowanie wielu metod na raz w prototypie
+  Person.prototype = {
+    sayHello: function () {
+      // asdasdasd
+    },
+    toString: function () {
+      // asdzfzs
+    }
+  }
 })();
