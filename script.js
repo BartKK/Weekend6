a3a937ae8bf2e380cd7bfd6e3e133e484f141bd0
BartKK <Bartkukulka@gmail.com>
2017-03-26 12:00:11 +0200

    Przywłaszczenie konstruktorów

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
-// Dziedziczenie konstruktorów
-
-
-//human
-  function VeryFriendlyConstructor () {
-    //sdfsdf
-  }
-
-  // js
-
-// !!VeryFriendlyConstructor jest podtypem Object!!
-// !!Object jest supertypem VeryFriendlyConstructor!!
-  VeryFriendlyConstructor.prototype = Object.create(Object.prototype, {
-    constructor: {
-      value: VeryFriendlyConstructor,
-      configurable: true,
-      writable: true,
-      enumerable: true
-    }
-  });
-
-
-//
+// Przywłaszczanie konstruktorów
 
 function Rectangle(length, width) {
-console.log('Został wywołany Rectangle')
-  this.length = length;
-  this.width = width;;
+this.length = length;
+this.width = width;
+
 }
 
 Rectangle.prototype.getArea = function () {
@@ -33,39 +11,21 @@ Rectangle.prototype.getArea = function () {
 };
 
 Rectangle.prototype.toString = function () {
-  return '[Rectangle' + this.length + ' ' + this.width + ']';
-}
-
-
-function Square(size) {
-  this.length = size;
-  this.width = size;
-}
-
-//
-
-Square.prototype = new Rectangle();
-Square.prototype.constructor = Square;
-
-Square.prototype.toString = function () {
-  return '[Square ' + this.length + ' na ' + this.width + ']'
+  return 'Rectangle ' + this.length + ' x ' + this.width;
 };
 
-var rect = new Rectangle(5, 10);
+function Square(size) {
+  Rectangle.call(this, size, size)
+}
+
+Square.prototype = Object.create(Rectangle.prototype, {
+  constructor: {
+    value: Square
+  }
+});
+
+Square.prototype.toString = function () {
+  return 'Square ' + this.length + ' x ' + this.width;
+};
+
 var square = new Square(6);
-
-
-console.log(rect.getArea()); // 50
-
-console.log(square.getArea()); // 36
-
-console.log(rect.toString()); // 'Rectangle'
-console.log(square.toString()); // 'Square'
-
-console.log(rect instanceof Rectangle); // true
-console.log(rect instanceof Square); // False
-console.log(rect instanceof Object); // true
-
-console.log(square instanceof Square); // True
-console.log(square instanceof Rectangle); // True
-console.log(square instanceof Object); // True
