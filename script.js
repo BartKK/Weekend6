1f8fddaffb24f7053b14f38011336e5a5e1e0e83
BartKK <Bartkukulka@gmail.com>
2017-03-26 09:54:38 +0200

    Metody dziedziczenia po Object.prototype

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-// Dziedziczenie - (prototype chaining)
+// Metody dziedziczone po Object.prototype
 
+// hasOwnProperty()
+// propertyIsEnumerable()
+// isPrototypeOf()
+// valueOf()
+// toString()
+
+///
+
+var now = new Date();
+var earlier = new Date(2017, 01, 01); // 2017-01-01
+
+console.log(now > earlier);
+
+// valueOf()
+
+var string = 'Hello world';
+var response = string.valueOf();
+console.log(response); // 'Hello world'
+
+
+// toString()
 
 var book = {
+  title: "Very interesting title"
+};
 
-  title: 'Prototype chaining - inheritance'
-}
+var message = 'Book' + book;
+console.log(message); // 'book = [object Object]'
 
-var prototype = Object.getPrototypeOf(book);
+//
+var book = {
+  title: 'Very interesting title',
+  toString: function () {
+    return '[Book with title of ' + this.title; + ']'
+  }
+};
 
-console.log(prototype == Object.prototype); // true
+var message = 'Book = ' + book;
+console.log(message); // Book = [Book with title of Very interesting title]
