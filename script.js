a965380a194b0d517918cfcb6ac8065259f5bf79
BartKK <Bartkukulka@gmail.com>
2017-03-26 10:47:53 +0200

    Dziedziczenie obiektów

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
-// Modyfikowanie prototypów
-
-Object.prototype.add = function (val) {
-  return this + val;
-};
+// Dziedziczenie obiektów
 
 var book = {
-  title: 'Some title'
+
+    price: 44
 };
 
-console.log(book.add(9)); // '[object Object]9'
-console.log('tytuł'.add(' książki')); //
+var book1 = Object.create(Object.prototype, {
+    title: {
+        value: 'Some another title',
+        enumerable: true,
+        configurable: true,
+        writable: true,
+    }
+});
 
-console.log(document.add(true));
+console.log(book);
+console.log(book1);
 
-// zabezpieczenie
+//
 
-var empty = {};
-for(var property in empty) {
-  if(empty.hasOwnProperty(property)) {
-    console.log(property);
+var person1 = {
+    name: 'Zuzia',
+    sayHello: function () {
+      console.log(this.name)
+    }
+};
+
+var person2 = Object.create(person1, {
+  name: {
+    value: 'Ola',
+    enumerable: true,
+    configurable: true,
+    writable: true,
+
   }
-}
+});
+
+person1.sayHello(); //
+person2.sayHello(); //
+
+console.log(person1.hasOwnProperty('sayHello')); // True
+console.log(person1.isPrototypeOf(person2)); // True
+console.log(person2.isPrototypeOf(person1)); // false
+console.log(person2.hasOwnProperty('sayHello')); // false
+
+//
+// unikanie kolizji nazw - konstruktor 'null'
+var emptyObject = Object.create(null);
+
+console.log('toString' in empyObject);
+console.log('valueOf' in emptyObject);
